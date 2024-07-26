5894025c0c9b16c44648403beeed39493eabb843
Cameron Candau <camcandau@gmail.com>
2024-07-25 20:29:03 -0700

    basic program CLI input is partially working

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use std::process::Command;
+use clap::Parser;
+use std::io::{self, Write};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "")]
+    volume: String,
+
+    #[arg(long, default_value = "")]
+    partition: String,
+
+    #[arg(long, default_value = "")]
+    password: String,
+
+    #[arg(long, default_value = "0")]
+    pim: u32,
+
+    #[arg(short, long, default_value = "")]
+    keyfile: String,
+}
+
+fn main() {
+    let mut args = Args::parse();
+
+    if args.volume.is_empty() {
+        println!("Volume is required");
+        print!("Volume: ");
+        std::io::stdout().flush().unwrap();
+        let mut volume = String::new();
+        io::stdin().read_line(&mut volume).unwrap();
+        args.volume = volume.trim().to_string();
+    }
+    
+    if args.partition.is_empty() {
+        println!("Partition is required");
+        print!("Partition: ");
+        std::io::stdout().flush().unwrap();
+        let mut partition = String::new();
+        io::stdin().read_line(&mut partition).unwrap();
+        args.partition = partition.trim().to_string();
+    }
+
+    if args.password.is_empty() {
+        println!("Password is required");
+        print!("Password: ");
+        std::io::stdout().flush().unwrap();
+        args.password = rpassword::read_password().unwrap();
+    }
+
+    if args.pim.eq(&0) {
+        println!("PIM is required");
+        print!("PIM: ");
+        std::io::stdout().flush().unwrap();
+        let input = rpassword::read_password().unwrap();
+        match input.parse::<u32>() {
+            Ok(pim) => args.pim = pim,
+            Err(_) => {
+                eprintln!("Invalid input. Please enter a valid number for PIM.");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    if args.keyfile.is_empty() {
+        println!("Keyfile is required");
+        print!("Keyfile: ");
+        std::io::stdout().flush().unwrap();
+        let mut keyfile = String::new();
+        io::stdin().read_line(&mut keyfile).unwrap();
+        args.keyfile = keyfile.trim().to_string();
+    }
+    
+    println!("The volume is: '{}'", args.volume);
+    println!("The partition is: '{}'", args.partition);
+    println!("The password is: '{}'", args.password);
+    println!("The PIM is: '{}'", args.pim);
+    println!("The keyfile is: '{}'", args.keyfile);
+
+    let output = Command::new("veracrypt")
+        .arg("--text")
+        .arg("--mount")
+        .arg(args.volume)
+        .arg(args.partition)
+        .arg("-p")
+        .arg(args.password)
+        .arg("--pim")
+        .arg(args.pim.to_string())
+        .arg("--keyfiles")
+        .arg(args.keyfile)
+        .arg("--protect-hidden")
+        .arg("no")
+        .spawn()
+        .expect("failed to execute process");
+}
